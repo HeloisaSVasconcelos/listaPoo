6cc6db7a1f67d532895583d9ac0b3a3f8be93753
HeloisaSVasconcelos <souzaheloisa277@gmail.com>
2026-04-03 13:07:32 -0300

    exercicio 2

--- src/exercicios/Carro.java
+++ src/exercicios/Carro.java
@@ -1,7 +1,16 @@
 package exercicios;
 
 public class Carro {
+
     String marca;
     String modelo;
     int ano;
+
+    public void exibir() {
+        System.out.printf("%s %s %d\n", marca, modelo, ano);
+    }
+
+    public int idadeDoCarro() {
+        return 2026 - ano;
+    }
 }
--- src/exercicios/Main.java
+++ src/exercicios/Main.java
@@ -1,18 +1,20 @@
 package exercicios;
 
 public class Main {
+
     public static void main(String[] args) {
-        Carro carro1 = new Carro();
-        carro1.marca = "Toyota";
-        carro1.modelo = "Corolla";
-        carro1.ano = 2022;
 
-        Carro carro2 = new Carro();
-        carro2.marca = "Honda";
-        carro2.modelo = "Civic";
-        carro2.ano = 2020;
+        Carro c1 = new Carro();
 
-        System.out.println("Carro 1: " + carro1.marca + " " + carro1.modelo + " " + carro1.ano);
-        System.out.println("Carro 2: " + carro2.marca + " " + carro2.modelo + " " + carro2.ano);
+        c1.marca = "Toyota";
+        c1.modelo = "Corolla";
+        c1.ano = 2020;
+
+        c1.exibir();
+
+        System.out.printf("Idade: %d\n", c1.idadeDoCarro());
+
+        // exibir() → não retorna valor (void)
+        // idadeDoCarro() → retorna valor (int)
     }
 }
