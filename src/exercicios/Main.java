e59b295512d692cc08f2e0c595b36d7ad22ab4db
HeloisaSVasconcelos <souzaheloisa277@gmail.com>
2026-04-03 13:10:57 -0300

    exercicio 3

--- src/exercicios/Main.java
+++ src/exercicios/Main.java
@@ -1,20 +1,34 @@
 package exercicios;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Carro c1 = new Carro();
+        ArrayList<Carro> lista = new ArrayList<>();
 
+        Carro c1 = new Carro();
         c1.marca = "Toyota";
         c1.modelo = "Corolla";
         c1.ano = 2020;
 
-        c1.exibir();
+        Carro c2 = new Carro();
+        c2.marca = "Honda";
+        c2.modelo = "Civic";
+        c2.ano = 2018;
 
-        System.out.printf("Idade: %d\n", c1.idadeDoCarro());
+        Carro c3 = new Carro();
+        c3.marca = "Ford";
+        c3.modelo = "Ka";
+        c3.ano = 2015;
 
-        // exibir() → não retorna valor (void)
-        // idadeDoCarro() → retorna valor (int)
+        lista.add(c1);
+        lista.add(c2);
+        lista.add(c3);
+
+        for (Carro c : lista) {
+            c.exibir();
+        }
     }
 }
