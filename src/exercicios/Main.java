27a40e0ff13aa5456236bf8caa63442f81afc3cd
HeloisaSVasconcelos <souzaheloisa277@gmail.com>
2026-04-03 12:37:38 -0300

    exercicio 1

---
+++ src/exercicios/Main.java
@@ -0,0 +1,19 @@
+package exercicios;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Carro c1 = new Carro();
+
+        c1.marca = "Toyota";
+        c1.modelo = "Corolla";
+        c1.ano = 2020;
+
+        c1.exibir();
+
+        System.out.printf("Idade: %d\n", c1.idadeDoCarro());
+
+        // exibir() → não retorna valor (void)
+        // idadeDoCarro() → retorna valor (int)
+    }
+}
