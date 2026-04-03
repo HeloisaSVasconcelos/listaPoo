d2e40c9c35f3001bc273ae874779a85958f5ad43
HeloisaSVasconcelos <souzaheloisa277@gmail.com>
2026-04-03 13:13:34 -0300

    exercicio 4

--- src/exercicios/Carro.java
+++ src/exercicios/Carro.java
@@ -2,15 +2,39 @@ package exercicios;
 
 public class Carro {
 
-    String marca;
-    String modelo;
-    int ano;
+    private String marca;
+    private String modelo;
+    private int ano;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        if (ano < 1886 || ano > 2026) {
+            System.out.printf("Ano inválido!\n");
+        } else {
+            this.ano = ano;
+        }
+    }
 
     public void exibir() {
         System.out.printf("%s %s %d\n", marca, modelo, ano);
-    }
-
-    public int idadeDoCarro() {
-        return 2026 - ano;
     }
 }
--- src/exercicios/Main.java
+++ src/exercicios/Main.java
@@ -6,27 +6,34 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Criando a lista para armazenar os objetos do tipo Carro
         ArrayList<Carro> lista = new ArrayList<>();
 
+        // Configurando o Carro 1 usando os Setters
         Carro c1 = new Carro();
-        c1.marca = "Toyota";
-        c1.modelo = "Corolla";
-        c1.ano = 2020;
+        c1.setMarca("Toyota");
+        c1.setModelo("Corolla");
+        c1.setAno(2020);
 
+        // Configurando o Carro 2 usando os Setters
         Carro c2 = new Carro();
-        c2.marca = "Honda";
-        c2.modelo = "Civic";
-        c2.ano = 2018;
+        c2.setMarca("Honda");
+        c2.setModelo("Civic");
+        c2.setAno(2018);
 
+        // Configurando o Carro 3 usando os Setters
         Carro c3 = new Carro();
-        c3.marca = "Ford";
-        c3.modelo = "Ka";
-        c3.ano = 2015;
+        c3.setMarca("Ford");
+        c3.setModelo("Ka");
+        c3.setAno(2015);
 
+        // Adicionando os objetos na lista
         lista.add(c1);
         lista.add(c2);
         lista.add(c3);
 
+        // Percorrendo a lista e chamando o método exibir de cada carro
+        System.out.println("--- Lista de Carros ---");
         for (Carro c : lista) {
             c.exibir();
         }
