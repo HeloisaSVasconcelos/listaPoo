bc652a69de2646ba02cc21c0d0a1a1a25fd31ce1
HeloisaSVasconcelos <souzaheloisa277@gmail.com>
2026-04-03 13:03:05 -0300

    exercicio 1

--- src/exercicios/Carro.java
+++ src/exercicios/Carro.java
@@ -1,16 +1,7 @@
 package exercicios;
 
 public class Carro {
-
     String marca;
     String modelo;
     int ano;
-
-    public void exibir() {
-        System.out.printf("%s %s %d\n", marca, modelo, ano);
-    }
-
-    public int idadeDoCarro() {
-        return 2026 - ano;
-    }
 }
--- src/exercicios/Main.java
+++ src/exercicios/Main.java
@@ -1,33 +1,18 @@
 package exercicios;
-import java.util.ArrayList;
 
 public class Main {
-
     public static void main(String[] args) {
+        Carro carro1 = new Carro();
+        carro1.marca = "Toyota";
+        carro1.modelo = "Corolla";
+        carro1.ano = 2022;
 
-        ArrayList<Carro> lista = new ArrayList<>();
+        Carro carro2 = new Carro();
+        carro2.marca = "Honda";
+        carro2.modelo = "Civic";
+        carro2.ano = 2020;
 
-        Carro c1 = new Carro();
-        c1.marca = "Toyota";
-        c1.modelo = "Corolla";
-        c1.ano = 2020;
-
-        Carro c2 = new Carro();
-        c2.marca = "Honda";
-        c2.modelo = "Civic";
-        c2.ano = 2018;
-
-        Carro c3 = new Carro();
-        c3.marca = "Ford";
-        c3.modelo = "Ka";
-        c3.ano = 2015;
-
-        lista.add(c1);
-        lista.add(c2);
-        lista.add(c3);
-
-        for (Carro c : lista) {
-            c.exibir();
-        }
+        System.out.println("Carro 1: " + carro1.marca + " " + carro1.modelo + " " + carro1.ano);
+        System.out.println("Carro 2: " + carro2.marca + " " + carro2.modelo + " " + carro2.ano);
     }
 }
